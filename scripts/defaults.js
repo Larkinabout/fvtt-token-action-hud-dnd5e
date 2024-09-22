5a095f074d66f01f656d13c3a6f57ed8cef60f30
Russell Larkin <russell.larkin@gmail.com>
2024-09-22 10:30:16 +0100

    First pass at dnd5e v4

--- scripts/defaults.js
+++ scripts/defaults.js
@@ -1,92 +1,92 @@
-import { GROUP } from './constants.js'
+import { GROUP } from "./constants.js";
 
 /**
  * Default categories and groups
  */
-export let DEFAULTS = null
+export let DEFAULTS = null;
 
-Hooks.once('tokenActionHudCoreApiReady', async (coreModule) => {
-    const groups = GROUP
-    Object.values(groups).forEach(group => {
-        group.name = coreModule.api.Utils.i18n(group.name)
-        group.listName = `Group: ${coreModule.api.Utils.i18n(group.name)}`
-    })
-    const groupsArray = Object.values(groups)
-    DEFAULTS = {
-        layout: [
-            {
-                nestId: 'inventory',
-                id: 'inventory',
-                name: coreModule.api.Utils.i18n('DND5E.Inventory'),
-                groups: [
-                    { ...groups.weapons, nestId: 'inventory_weapons' },
-                    { ...groups.equipment, nestId: 'inventory_equipment' },
-                    { ...groups.consumables, nestId: 'inventory_consumables' },
-                    { ...groups.tools, nestId: 'inventory_tools' },
-                    { ...groups.containers, nestId: 'inventory_containers' },
-                    { ...groups.loot, nestId: 'inventory_loot' }
-                ]
-            },
-            {
-                nestId: 'features',
-                id: 'features',
-                name: coreModule.api.Utils.i18n('DND5E.Features'),
-                groups: [
-                    { ...groups.activeFeatures, nestId: 'features_active-features' },
-                    { ...groups.passiveFeatures, nestId: 'features_passive-features' }
-                ]
-            },
-            {
-                nestId: 'spells',
-                id: 'spells',
-                name: coreModule.api.Utils.i18n('ITEM.TypeSpellPl'),
-                groups: [
-                    { ...groups.atWillSpells, nestId: 'spells_at-will-spells' },
-                    { ...groups.innateSpells, nestId: 'spells_innate-spells' },
-                    { ...groups.pactSpells, nestId: 'spells_pact-spells' },
-                    { ...groups.cantrips, nestId: 'spells_cantrips' },
-                    { ...groups._1stLevelSpells, nestId: 'spells_1st-level-spells' },
-                    { ...groups._2ndLevelSpells, nestId: 'spells_2nd-level-spells' },
-                    { ...groups._3rdLevelSpells, nestId: 'spells_3rd-level-spells' },
-                    { ...groups._4thLevelSpells, nestId: 'spells_4th-level-spells' },
-                    { ...groups._5thLevelSpells, nestId: 'spells_5th-level-spells' },
-                    { ...groups._6thLevelSpells, nestId: 'spells_6th-level-spells' },
-                    { ...groups._7thLevelSpells, nestId: 'spells_7th-level-spells' },
-                    { ...groups._8thLevelSpells, nestId: 'spells_8th-level-spells' },
-                    { ...groups._9thLevelSpells, nestId: 'spells_9th-level-spells' }
-                ]
-            },
-            {
-                nestId: 'attributes',
-                id: 'attributes',
-                name: coreModule.api.Utils.i18n('DND5E.Attributes'),
-                groups: [
-                    { ...groups.abilities, nestId: 'attributes_abilities' },
-                    { ...groups.skills, nestId: 'attributes_skills' }
-                ]
-            },
-            {
-                nestId: 'effects',
-                id: 'effects',
-                name: coreModule.api.Utils.i18n('DND5E.Effects'),
-                groups: [
-                    { ...groups.temporaryEffects, nestId: 'effects_temporary-effects' },
-                    { ...groups.passiveEffects, nestId: 'effects_passive-effects' },
-                    { ...groups.conditions, nestId: 'effects_conditions' }
-                ]
-            },
-            {
-                nestId: 'utility',
-                id: 'utility',
-                name: coreModule.api.Utils.i18n('tokenActionHud.utility'),
-                groups: [
-                    { ...groups.combat, nestId: 'utility_combat' },
-                    { ...groups.token, nestId: 'utility_token' },
-                    { ...groups.rests, nestId: 'utility_rests' },
-                    { ...groups.utility, nestId: 'utility_utility' }
-                ]
-            }
-        ],
-        groups: groupsArray
-    }
-})
+Hooks.once("tokenActionHudCoreApiReady", async coreModule => {
+  const groups = GROUP;
+  Object.values(groups).forEach(group => {
+    group.name = coreModule.api.Utils.i18n(group.name);
+    group.listName = `Group: ${coreModule.api.Utils.i18n(group.name)}`;
+  });
+  const groupsArray = Object.values(groups);
+  DEFAULTS = {
+    layout: [
+      {
+        nestId: "inventory",
+        id: "inventory",
+        name: coreModule.api.Utils.i18n("DND5E.Inventory"),
+        groups: [
+          { ...groups.weapons, nestId: "inventory_weapons" },
+          { ...groups.equipment, nestId: "inventory_equipment" },
+          { ...groups.consumables, nestId: "inventory_consumables" },
+          { ...groups.tools, nestId: "inventory_tools" },
+          { ...groups.containers, nestId: "inventory_containers" },
+          { ...groups.loot, nestId: "inventory_loot" }
+        ]
+      },
+      {
+        nestId: "features",
+        id: "features",
+        name: coreModule.api.Utils.i18n("DND5E.Features"),
+        groups: [
+          { ...groups.activeFeatures, nestId: "features_active-features" },
+          { ...groups.passiveFeatures, nestId: "features_passive-features" }
+        ]
+      },
+      {
+        nestId: "spells",
+        id: "spells",
+        name: coreModule.api.Utils.i18n("TYPES.Item.spellPl"),
+        groups: [
+          { ...groups.atWillSpells, nestId: "spells_at-will-spells" },
+          { ...groups.innateSpells, nestId: "spells_innate-spells" },
+          { ...groups.pactSpells, nestId: "spells_pact-spells" },
+          { ...groups.cantrips, nestId: "spells_cantrips" },
+          { ...groups._1stLevelSpells, nestId: "spells_1st-level-spells" },
+          { ...groups._2ndLevelSpells, nestId: "spells_2nd-level-spells" },
+          { ...groups._3rdLevelSpells, nestId: "spells_3rd-level-spells" },
+          { ...groups._4thLevelSpells, nestId: "spells_4th-level-spells" },
+          { ...groups._5thLevelSpells, nestId: "spells_5th-level-spells" },
+          { ...groups._6thLevelSpells, nestId: "spells_6th-level-spells" },
+          { ...groups._7thLevelSpells, nestId: "spells_7th-level-spells" },
+          { ...groups._8thLevelSpells, nestId: "spells_8th-level-spells" },
+          { ...groups._9thLevelSpells, nestId: "spells_9th-level-spells" }
+        ]
+      },
+      {
+        nestId: "attributes",
+        id: "attributes",
+        name: coreModule.api.Utils.i18n("DND5E.Attributes"),
+        groups: [
+          { ...groups.abilities, nestId: "attributes_abilities" },
+          { ...groups.skills, nestId: "attributes_skills" }
+        ]
+      },
+      {
+        nestId: "effects",
+        id: "effects",
+        name: coreModule.api.Utils.i18n("DND5E.Effects"),
+        groups: [
+          { ...groups.temporaryEffects, nestId: "effects_temporary-effects" },
+          { ...groups.passiveEffects, nestId: "effects_passive-effects" },
+          { ...groups.conditions, nestId: "effects_conditions" }
+        ]
+      },
+      {
+        nestId: "utility",
+        id: "utility",
+        name: coreModule.api.Utils.i18n("tokenActionHud.utility"),
+        groups: [
+          { ...groups.combat, nestId: "utility_combat" },
+          { ...groups.token, nestId: "utility_token" },
+          { ...groups.rests, nestId: "utility_rests" },
+          { ...groups.utility, nestId: "utility_utility" }
+        ]
+      }
+    ],
+    groups: groupsArray
+  };
+});
--- scripts/magic-items-extender.js
+++ scripts/magic-items-extender.js
@@ -1,101 +1,101 @@
-export let MagicItemActionHandlerExtender = null
+export let MagicItemActionHandlerExtender = null;
 
-Hooks.once('tokenActionHudCoreApiReady', async (coreModule) => {
-    MagicItemActionHandlerExtender = class MagicItemActionHandlerExtender extends coreModule.api.ActionHandlerExtender {
-        constructor (actionHandler) {
-            super()
-            this.actionHandler = actionHandler
-            this.actor = null
-        }
+Hooks.once("tokenActionHudCoreApiReady", async coreModule => {
+  MagicItemActionHandlerExtender = class MagicItemActionHandlerExtender extends coreModule.api.ActionHandlerExtender {
+    constructor(actionHandler) {
+      super();
+      this.actionHandler = actionHandler;
+      this.actor = null;
+    }
 
-        /**
+    /**
      * Extend the action list
      */
-        async extendActionHandler () {
-            this.actor = this.actionHandler.actor
-            if (!this.actor) return
+    async extendActionHandler() {
+      this.actor = this.actionHandler.actor;
+      if (!this.actor) return;
 
-            const actor = await MagicItems.actor(this.actor.id)
+      const actor = await MagicItems.actor(this.actor.id);
 
-            if (!actor) return
+      if (!actor) return;
 
-            const magicItems = actor.items ?? []
+      const magicItems = actor.items ?? [];
 
-            if (magicItems.length === 0) return
+      if (magicItems.length === 0) return;
 
-            const parentGroupId = 'magic-items'
-            const parentGroupType = 'system'
-            const parentGroupData = {
-                id: parentGroupId,
-                type: parentGroupType
-            }
+      const parentGroupId = "magic-items";
+      const parentGroupType = "system";
+      const parentGroupData = {
+        id: parentGroupId,
+        type: parentGroupType
+      };
 
-            magicItems.forEach((magicItem) => {
-                if (magicItem.attuned && !this._isItemAttuned(magicItem)) return
-                if (magicItem.equipped && !this._isItemEquipped(magicItem)) return
+      magicItems.forEach(magicItem => {
+        if (magicItem.attuned && !this._isItemAttuned(magicItem)) return;
+        if (magicItem.equipped && !this._isItemEquipped(magicItem)) return;
 
-                const groupId = `magic-items_${magicItem.id}`
-                const groupName = magicItem.name
-                const groupType = 'system-derived'
-                const groupInfo1 = `${magicItem.uses}/${magicItem.charges}`
-                const groupData = {
-                    id: groupId,
-                    name: groupName,
-                    type: groupType,
-                    info1: groupInfo1
-                }
+        const groupId = `magic-items_${magicItem.id}`;
+        const groupName = magicItem.name;
+        const groupType = "system-derived";
+        const groupInfo1 = `${magicItem.uses}/${magicItem.charges}`;
+        const groupData = {
+          id: groupId,
+          name: groupName,
+          type: groupType,
+          info1: groupInfo1
+        };
 
-                // Add group to HUD
-                this.actionHandler.addGroup(groupData, parentGroupData)
+        // Add group to HUD
+        this.actionHandler.addGroup(groupData, parentGroupData);
 
-                const actions = magicItem.ownedEntries.map((entry) => {
-                    const effect = entry.item
-                    const id = effect.id
-                    const name = effect.name
-                    const encodedValue = [
-                        'magicItem',
-                        `${magicItem.id}>${id}`
-                    ].join('|')
-                    const img = coreModule.api.Utils.getImage(effect)
-                    const info1 = effect.consumption
-                    const info2 = (effect.baseLevel) ? `${coreModule.api.Utils.i18n('DND5E.AbbreviationLevel')} ${effect.baseLevel}` : ''
-                    return {
-                        id,
-                        name,
-                        encodedValue,
-                        img,
-                        info1,
-                        info2,
-                        selected: true
-                    }
-                })
+        const actions = magicItem.ownedEntries.map(entry => {
+          const effect = entry.item;
+          const id = effect.id;
+          const name = effect.name;
+          const encodedValue = [
+            "magicItem",
+            `${magicItem.id}>${id}`
+          ].join("|");
+          const img = coreModule.api.Utils.getImage(effect);
+          const info1 = effect.consumption;
+          const info2 = (effect.baseLevel) ? `${coreModule.api.Utils.i18n("DND5E.AbbreviationLevel")} ${effect.baseLevel}` : "";
+          return {
+            id,
+            name,
+            encodedValue,
+            img,
+            info1,
+            info2,
+            selected: true
+          };
+        });
 
-                // Add actions to action list
-                this.actionHandler.addActions(actions, groupData)
-            })
-        }
+        // Add actions to action list
+        this.actionHandler.addActions(actions, groupData);
+      });
+    }
 
-        /**
+    /**
      * Whether the magic item is equipped or not
      * @param {object} magicItem The item
      * @returns {boolean}
      */
-        _isItemEquipped (magicItem) {
-            return magicItem.item.system.equipped
-        }
+    _isItemEquipped(magicItem) {
+      return magicItem.item.system.equipped;
+    }
 
-        /**
+    /**
      * Whether the magic items is attuned or not
      * @param {object} magicItem The item
      * @returns {boolean}
      */
-        _isItemAttuned (magicItem) {
-            const attunement = magicItem.item.system.attunment
-            const attunementRequired = CONFIG.DND5E.attunementTypes?.REQUIRED ?? 1
+    _isItemAttuned(magicItem) {
+      const attunement = magicItem.item.system.attunment;
+      const attunementRequired = CONFIG.DND5E.attunementTypes?.REQUIRED ?? 1;
 
-            if (attunement === attunementRequired) return false
+      if (attunement === attunementRequired) return false;
 
-            return true
-        }
+      return true;
     }
-})
+  };
+});
